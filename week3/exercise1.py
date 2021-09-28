61ec3b19136c9387260f530953faf02f656ae920
cascadeth <cascadeth@gmail.com>
2021-09-28 13:40:32 -0700

    Open + readlines directly into the foreach loop

--- week3/exercise1.py
+++ week3/exercise1.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
-data_lines = open("show_vlan.txt").readlines()
-
-# vlans = [(line.split()[0], line.split()[1]) for line in data_lines]
-
 vlans = []
 
-for line in data_lines:
+for line in open("show_vlan.txt").readlines():
     if not line.split()[0].isdigit():
         continue
     vlans.append((line.split()[0], line.split()[1]))
